7acb10c9ff70c785edc4475a150b6525efe876bd
mwetmore <mwetmore@gmail.com>
2020-06-03 23:06:39 -0400

    Tweaking config.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     // Output directory: _site
   
     // Copy `img/` to `_site/img`
-    eleventyConfig.addPassthroughCopy("img");
+   // eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("admin");
     
     // Copy `css/fonts/` to `_site/css/fonts`
